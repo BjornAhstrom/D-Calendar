aaa25a136d619ce883e970ef2335fbe08bfa00dc
bjornahstrom <ahstrom_81@hotmail.com>
2023-08-28 19:03:51 +0200

    Added swedish weekdays and month names

--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -1,4 +1,47 @@
-import { Calendar } from "react-native-calendars";
+import { Calendar, LocaleConfig } from "react-native-calendars";
+
+LocaleConfig.locales["sv"] = {
+  monthNames: [
+    "Januari",
+    "Februari",
+    "Mars",
+    "April",
+    "Maj",
+    "Juni",
+    "Juli",
+    "Augusti",
+    "September",
+    "Oktober",
+    "November",
+    "December",
+  ],
+  monthNamesShort: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Maj",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Dec",
+  ],
+  dayNames: [
+    "Söndag",
+    "Måndag",
+    "Tisdag",
+    "Onsdag",
+    "Torsdag",
+    "Fredag",
+    "Lördag",
+  ],
+  dayNamesShort: ["Sön", "Mån", "Tis", "Ons", "Tor", "Fre", "Lör"],
+  today: "Idag",
+};
+LocaleConfig.defaultLocale = "sv";
 
 const CustomCalendar = () => {
   return <Calendar firstDay={1} />;
