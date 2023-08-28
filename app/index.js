3dc216b2da6be3531433a03c880bc5ba46ff5d52
bjornahstrom <ahstrom_81@hotmail.com>
2023-08-28 19:00:19 +0200

    Installed react native calendar

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
+import CustomCalendar from "../components/calendar/calendar";
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
+      <CustomCalendar />
     </View>
   );
 };
---
+++ components/calendar/calendar.js
@@ -0,0 +1,7 @@
+import { Calendar } from "react-native-calendars";
+
+const CustomCalendar = () => {
+  return <Calendar firstDay={1} />;
+};
+
+export default CustomCalendar;
